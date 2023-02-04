32e0cb5683beb0589152c2f438419d47aba55c24
de4life <ageev_artur@ukr.net>
2023-02-04 11:16:20 +0200

    Initial Commit

---
+++ SearchMusic/SearchMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchMusicApp.swift
+//  SearchMusic
+//
+//  Created by Артур Агеев on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
