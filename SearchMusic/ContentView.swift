4ba22bf521fcf2d7544ae144f5ec61c6b1e1e4f0
de4life <ageev_artur@ukr.net>
2023-02-07 11:51:38 +0200

    add UI main screen

--- SearchMusic/ContentView.swift
+++ SearchMusic/ContentView.swift
@@ -9,9 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                SearchBar()
+                EmptyStateView()
+            }.navigationTitle("Music Search")
+        }
     }
+}
+
+struct EmptyStateView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Image(systemName: "music.note")
+                .font(.system(size: 85))
+                .padding(.bottom)
+            Text("Start searching music.....")
+                .font(.title)
+            Spacer()
+        }
+        .padding()
+        .foregroundColor(Color(.systemIndigo))
+    }
+}
+
+struct SearchBar: UIViewRepresentable {
+    typealias UIViewType = UISearchBar
+    
+    func makeUIView(context: Context) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        searchBar.placeholder = "Type a song, artist, or album name..."
+        return searchBar
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: Context) {
+        <#code#>
+    }
+    
+    func makeCoordinator() -> SearchBarCoordinator {
+        return SearchBarCoordinator()
+        
+    }
+    
+    class SearchBarCoordinator: NSObject, UISearchBarDelegate {
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
