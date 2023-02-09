8fe34b1e12820f8c230487b71f39e628e6f62b18
de4life <ageev_artur@ukr.net>
2023-02-09 12:31:38 +0200

    call

--- SearchMusic/SearchMusicApp.swift
+++ SearchMusic/SearchMusicApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SearchMusicApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: SongListViewModel())
         }
     }
 }
